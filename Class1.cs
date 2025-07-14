55b06cb6b80628422862e068394f561ef70ff02a
Nicholas <nicholascarta@gmail.com>
2025-07-14 14:17:25 -0400

    First Commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedBioWaste
+{
+    public class Class1
+    {
+    }
+}
