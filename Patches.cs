72af2b9b725cdfe0c8a7556285dabaf9da4a5199
Nicholas <nicholascarta@gmail.com>
2025-07-14 18:37:50 -0400

    Added new exposure type: Bacteria

--- Patches.cs
+++ Patches.cs
@@ -9,37 +9,42 @@ using UnityEngine;
 
 namespace AdvancedBioWaste
 {
-    public class Patches
-    {
-        [HarmonyPatch(typeof(TUNING.GERM_EXPOSURE))]
-        [HarmonyPatch(MethodType.StaticConstructor)]
-        public class GermExposurePatch
-        {
-            public static void Postfix()
-            {
-                var exposureList = TUNING.GERM_EXPOSURE.TYPES.ToList();
 
-                ExposureType customExposure = new ExposureType()
-                {
-                    germ_id = "Bacteria",
-                    sickness_id = "BacterialInfection",
-                    exposure_threshold = 100,
-                    excluded_traits = new List<string>() { "IronGut" },
-                    base_resistance = 2,
-                    excluded_effects = new List<string>()
+    public class MyMod : KMod.UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            harmony.PatchAll();
+            Debug.Log("[MyMod] Harmony patching...");
+
+            Debug.Log("Running exposure patch...");
+
+            var exposureList = TUNING.GERM_EXPOSURE.TYPES.ToList();
+
+            ExposureType customExposure = new ExposureType()
+            {
+                germ_id = "Bacteria",
+                sickness_id = "BacterialInfection",
+                exposure_threshold = 100,
+                excluded_traits = new List<string>() { "IronGut" },
+                base_resistance = 2,
+                excluded_effects = new List<string>()
                     {
                         "BacterialInfectionRecovery"
                     }
-                };
-                exposureList.Add(customExposure);
+            };
 
-                TUNING.GERM_EXPOSURE.TYPES = exposureList.ToArray();
+            exposureList.Add(customExposure);
+            TUNING.GERM_EXPOSURE.TYPES = exposureList.ToArray();
 
-                foreach (var exposure in TUNING.GERM_EXPOSURE.TYPES)
-                {
-                    Debug.Log($"ExposureType ID: {exposure.germ_id}, Sickness: {exposure.sickness_id}");
-                }
+            Debug.Log("About to run the for each loop....");
+
+            foreach (var exposure in TUNING.GERM_EXPOSURE.TYPES)
+            {
+                Debug.Log($"ExposureType ID: {exposure.germ_id}, Sickness: {exposure.sickness_id}");
             }
-        }
+
+        }       
     }
 }
